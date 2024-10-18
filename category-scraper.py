2de7a110755ec31c727eee181506d5c2896c3e0a
Edwin Jakobs <edwin@rndr.studio>
2024-10-18 10:10:54 +0200

    Add API key headers

--- category-scraper.py
+++ category-scraper.py
@@ -3,6 +3,9 @@ import json
 import requests
 import os.path
 
+api_key = None
+api_username = "system"
+
 base_url = "https://discourse.processing.org"
 
 search_base_url = f"{base_url}/search.json"
@@ -20,12 +23,14 @@ while True:
     else:
         break
 
-
 print(f"starting from page {page}")
 
 while True:
     search_url = f"https://discourse.processing.org/search.json?q=category:{category}%20before:2024-01-01%20after:2023-01-01?page={page}"
-    r = requests.get(search_url)
+
+    headers = {"Api-Username": api_username, "Api-Key": api_key} if api_key is not None else {}
+
+    r = requests.get(search_url, headers=headers)
     if r.status_code == 429:
         print(f"Rate limit reached, sleeping for {r.headers['Retry-After']}")
         time.sleep(int(r.headers['Retry-After']))
